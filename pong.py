45e94c050c21660ea7bf392ef506bc43d64a2101
JKrecisz <jakub.krecisz@icloud.com>
2022-01-01 22:37:19 +0100

    Added difficulty and changed the size of the window

--- pong.py
+++ pong.py
@@ -3,14 +3,16 @@
 
 import turtle
 
-score_player_a = 0
-score_player_b = 0
+# Player setup
+score_player_a, score_player_b = 0, 0
+print("- 1 - easy\n- 2 - medium\n- 3 - hard\n")
+difficulty = int(input("Difficulty: "))
 
 # Window Setup
 window = turtle.Screen()
 window.title("Pong game by @JKrecisz")
 window.bgcolor("black")
-window.setup(width=800, height=600)
+window.setup(width=1000, height=800)
 window.tracer(0)
 
 # Paddle 1
@@ -20,7 +22,7 @@ paddle1.shape("square")
 paddle1.color("white")
 paddle1.shapesize(stretch_wid=5, stretch_len=1)
 paddle1.penup()
-paddle1.goto(-350, 0)
+paddle1.goto(-450, 0)
 
 # Paddle 2
 paddle2 = turtle.Turtle()
@@ -29,7 +31,7 @@ paddle2.shape("square")
 paddle2.color("white")
 paddle2.shapesize(stretch_wid=5, stretch_len=1)
 paddle2.penup()
-paddle2.goto(350, 0)
+paddle2.goto(450, 0)
 
 # Ball
 ball = turtle.Turtle()
@@ -38,8 +40,16 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 8
-ball.dy = -8
+
+if difficulty == 3:
+    ball.dx = 12
+    ball.dy = -12
+elif difficulty == 2:
+    ball.dx = 9
+    ball.dy = -9
+else:
+    ball.dx = 7
+    ball.dy = -7
 
 # Pen
 pen = turtle.Turtle()
@@ -47,8 +57,8 @@ pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 24, "normal"))
+pen.goto(0, 350)
+pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 26, "normal"))
 
 # Function
 def paddle1_up():
@@ -89,30 +99,30 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Border checking
-    if ball.ycor() > (window.window_height()/2)-10:
+    if ball.ycor() > (window.window_height()/2)-20:
         ball.dy *= -1
 
-    if ball.ycor() < -((window.window_height()/2)-10):
+    if ball.ycor() < -((window.window_height()/2)-20):
         ball.dy *= -1
 
-    if ball.xcor() > (window.window_width()/2)-10:
+    if ball.xcor() > (window.window_width()/2)-20:
         ball.goto(0, 0)
         ball.dx *= -1
         score_player_a += 1
 
-    if ball.xcor() < -((window.window_width()/2)-10):
+    if ball.xcor() < -((window.window_width()/2)-20):
         ball.goto(0, 0)
         ball.dx *= -1
         score_player_b += 1
 
     # Paddle and ball colisions
 
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle2.ycor() + 50 and ball.ycor() > paddle2.ycor() - 50):
-        ball.setx(330)
+    if (ball.xcor() > 450 and ball.xcor() < 470) and (ball.ycor() < paddle2.ycor() + 50 and ball.ycor() > paddle2.ycor() - 50):
+        ball.setx(420)
         ball.dx *= -1
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle1.ycor() + 50 and ball.ycor() > paddle1.ycor() - 50):
-        ball.setx(-330)
+    if (ball.xcor() < -450 and ball.xcor() > -470) and (ball.ycor() < paddle1.ycor() + 50 and ball.ycor() > paddle1.ycor() - 50):
+        ball.setx(-420)
         ball.dx *= -1
 
     pen.clear()
